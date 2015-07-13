bb024b6c414c256737f8039a52fc9f4d61863fea
Jochem van de Weerthof <j.vandeweerthof@kwebbl.com>
2015-07-13 17:05:30 +0300

    Revert "Allow monitoring of multiple replications"
    This reverts commit 2ee1031a0409f6d58648adb81a23147b088c64af.

--- npm-replicator-monitor.js
+++ npm-replicator-monitor.js
@@ -13,68 +13,46 @@ if (!argv.c || !fs.existsSync(argv.c)) {
 
 
 var config     = JSON.parse(fs.readFileSync(argv.c))
-  , checks = []
+  , replicator = new CouchReplicator(config.couchUrl, config.couchUser, config.couchPass, config.db)
+  , lastCheckpoint
 
-/**
- * Runs check for replication identified by [id]
- * @function check
- * @param {number} id - The [id] of the check
- * @returns {Void}
- */
-function check (id) {
-  checks[id].replicator.status(function (err, status) {
+
+function check () {
+  replicator.status(function (err, status) {
     if (err)
-      return console.error('Got error from status: %s [%s %s]', err.message, checks[id].config.db, checks[id].config.couchUrl)
+      return console.error('Got error from status:', err.message)
+
     if (status.checkpointed_source_seq == status.source_seq) {
-      console.log('Replication up to date [%s %s]', checks[id].config.db, checks[id].config.couchUrl)
-      checks[id].lastCheckpoint = null
+      console.log('Replication up to date')
+      lastCheckpoint = null
       return // all good!
     }
 
-    if (status.checkpointed_source_seq != checks[id].lastCheckpoint) {
-      console.log('Checkpoint changed since last check, was %d now %d [%s %s]', checks[id].lastCheckpoint, status.checkpointed_source_seq, checks[id].config.db, checks[id].config.couchUrl)
-      checks[id].lastCheckpoint = status.checkpointed_source_seq
+    if (status.checkpointed_source_seq != lastCheckpoint) {
+      console.log('Checkpoint changed since last check, was', lastCheckpoint, 'now', status.checkpointed_source_seq)
+      lastCheckpoint = status.checkpointed_source_seq
       return // something has changed
     }
 
     // nothing has changed, time to bump!
-    console.log('Giving the replicator a jolt... [%s %s]', checks[id].config.db, checks[id].config.couchUrl)
-    checks[id].lastCheckpoint = null
-    checks[id].replicator.del(function (err) {
+    console.log('Giving the replicator a jolt...')
+    lastCheckpoint = null
+    replicator.del(function (err) {
       if (err)
-        return console.error('Got error deleting replication doc: %s [%s %s]', err.message, checks[id].config.db, checks[id].config.couchUrl)
+        return console.error('Got error deleting replication doc:', err.message)
 
       setTimeout(function () {
         // Add a new Date so we ensure the rev is different (helps with determinism)
-        var doc = xtend(checks[id].config.replicationDoc, { created_at_date: new Date() })
-        checks[id].replicator.put(doc, function (err) {
+        var doc = xtend(config.replicationDoc, { created_at_date: new Date() })
+        replicator.put(doc, function (err) {
           if (err)
-            return console.error('Error adding replication doc:%s [%s %s]', err.message, checks[id].config.db, checks[id].config.couchUrl)
+            return console.error('Error adding replication doc:', err.message)
         })
       }, 2000) // give it a moment to think
     })
   })
 }
 
-/**
- * Starts the application
- * @function main
- * @returns {Void}
- */
-function main () {
-  if (!Array.isArray(config))
-    config = [config] // Maintain backward compatibility for single replications in config
 
-  for (var id in config) {
-  	checks[id] = {}
-    checks[id].replicator = new CouchReplicator(config[id].couchUrl, config[id].couchUser, config[id].couchPass, config[id].db)
-    checks[id].lastCheckpoint = null
-    checks[id].config = config[id]
-    setInterval(check.bind(null, id), config[id].checkInterval)
-    check(id)
-    console.log('Started replication checks [%s %s]', config[id].db, config[id].couchUrl);
-  }
-  return;
-}
-
-main()
+setInterval(check, config.checkInterval)
+check()
